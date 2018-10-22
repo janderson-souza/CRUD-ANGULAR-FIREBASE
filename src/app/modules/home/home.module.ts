be6b046f363cdb05b73d302414abb18df8ef0960
Janderson Souza <jandersonsouza.ti@gmail.com>
2018-10-21 23:19:57 -0300

    Title component
    Criando componente para titulos.

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './page/home.component';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    SharedModule
   ],
   declarations: [HomeComponent]
 })
