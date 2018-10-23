9527ac3f500c18d1b3d8d61e8a86a8505ee76080
Janderson Souza <jandersonsouza.ti@gmail.com>
2018-10-22 21:27:54 -0300

    Style for title component

--- src/app/shared/components/title/title.component.ts
+++ src/app/shared/components/title/title.component.ts
@@ -1,13 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostBinding, Input } from '@angular/core';
 
 @Component({
   selector: 'app-title',
   templateUrl: './title.component.html',
-  styleUrls: ['./title.component.scss']
+  styleUrls: ['./title.component.scss'],
 })
 export class TitleComponent implements OnInit {
-
+  
   constructor() { }
+
+  @Input('icon') icon;
 
   ngOnInit() {
   }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TitleComponent } from './components/title/title.component';
+import { MatIconModule } from '@angular/material';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    MatIconModule
   ],
   declarations: [ TitleComponent ],
   exports: [ TitleComponent ]
