b178823a7dd0bcd1815d5f3b05bec0e69547d07e
Janderson Souza <jandersonsouza.ti@gmail.com>
2018-10-22 22:53:56 -0300

    Theming for Angular Material app
    Create theming for material io

--- src/app/modules/product/product.module.ts
+++ src/app/modules/product/product.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { ProductRoutingModule } from './product-routing.module';
 import { QueryProductComponent } from './query-page/query-product.component';
 import { FormProductComponent } from './form-page/form-product.component';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
   imports: [
     CommonModule,
     ProductRoutingModule,
+    SharedModule
   ],
   declarations: [QueryProductComponent, FormProductComponent]
 })
